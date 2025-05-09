5f856930d16be13cae74a2c12e62181004dfd392
Özlem Demir <152503347+ozlemxdemir@users.noreply.github.com>
2025-05-09 20:00:03 +0300

    Add files via upload

---
+++ hafta11.py
@@ -0,0 +1,49 @@
+class Graph():
+    def __init__(self, vertices):
+        self.V = vertices
+        self.graph = [[0 for column in range(vertices)]
+                      for row in range(vertices)]
+
+    def printSolution(self, dist):
+        print("Vertex \t Distance from Source")
+        for node in range(self.V):
+            print(node, "\t\t", dist[node])
+
+    def minDistance(self, dist, sptSet):
+        min = 1e7
+        for v in range(self.V):
+            if dist[v] < min and sptSet[v] == False:
+                min = dist[v]
+                min_index = v
+        return min_index
+
+    def dijkstra(self, src):
+        dist = [1e7] * self.V
+        dist[src] = 0
+        sptSet = [False] * self.V
+
+        for cout in range(self.V):
+            u = self.minDistance(dist, sptSet)
+            sptSet[u] = True
+
+            for v in range(self.V):
+                if (self.graph[u][v] > 0 and
+                   sptSet[v] == False and
+                   dist[v] > dist[u] + self.graph[u][v]):
+                    dist[v] = dist[u] + self.graph[u][v]
+
+        self.printSolution(dist)
+
+
+
+g = Graph(5)
+g.graph = [
+    [0, 3, 5, 1, 0], 
+    [3, 0, 1, 6, 0],  
+    [5, 1, 0, 4, 6],  
+    [1, 6, 4, 0, 4],  
+    [0, 0, 6, 4, 0]   
+]
+
+g.dijkstra(0)
+
